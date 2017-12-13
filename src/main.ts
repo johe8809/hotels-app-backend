515253949f115fe8e485650980ac6970632e9b1e
Yonnathan Henao Restrepo <yhenao@MacBook-Pro-de-Yonnathan.local>
2017-12-13 09:35:37 -0500

    Se cambia la cadena de conexion de la base de datos mongo

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ const config = {
     port: process.env.PORT || 3000,
 
     // database
-    db: 'mongodb://localhost/hotelsdb',
+    //db: 'mongodb://localhost/hotelsdb',
+    db: 'mongodb://user_db:4lmundo@ds139446.mlab.com:39446/hotelsdb'
 
 
 
